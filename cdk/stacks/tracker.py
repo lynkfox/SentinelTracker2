2427cecf636d86bc673e008d3bb6e3a6a3aacdb6
Lynkfox <lynkfox@gmail.com>
2021-05-21 22:17:31 -0400

    Inital Tracker Stack

---
+++ cdk/stacks/tracker.py
@@ -0,0 +1,59 @@
+from aws_cdk import (
+    core as cdk,
+    aws_lambda as aws_lambda,
+    aws_dynamodb as dynamodb,
+    aws_apigateway as api,
+    aws_iam as iam
+    
+)
+import os
+from pathlib import Path
+
+base_directory = Path(__file__).parents[2]
+
+
+class Tracker(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        
+        ################################
+        # DynamoDBs                    #
+        ################################
+        
+        tracker_db = dynamodb.Table(
+            self, "TrackerDynamo",
+            table_name="SentinelsCardGameTracker",
+            partition_key={ 'name': 'pk', 'type': dynamodb.AttributeType.STRING},
+            sort_key={ 'name': 'sk', 'type': dynamodb.AttributeType.STRING}
+        )
+        
+        ################################
+        # Lambdas                      #
+        ################################
+        
+        ###########
+        # Env Var #
+        ###########
+        
+        lambda_environment_variables = {
+            'TRACKER_DYNAMO': tracker_db.table_name
+        }
+        
+        entity_lambda = aws_lambda.Function(
+            self, "EntityLambda",
+            function_name="Entity_Lambda",
+            runtime=aws_lambda.Runtime.PYTHON_3_8,
+            handler='entity_handler.entity_lambda_handler',
+            code=aws_lambda.Code.asset(os.path.join(base_directory, "lambdas/entity_lambda")),
+            environment=lambda_environment_variables
+        )
+        
+        ################################
+        # APIs                         #
+        ################################
+        
+        api.LambdaRestApi(
+            self, 'TrackerEndpoint',
+            handler=entity_lambda,
+        )
---
+++ lambdas/entity_lambda/entity_handler.py
@@ -0,0 +1,6 @@
+def entity_lambda_handler(event:dict, context:dict) -> dict:
+    '''
+    Lambda Handler for the Entity API end points
+    '''
+    
+    return { "message": "Entiy Handler Online"}
