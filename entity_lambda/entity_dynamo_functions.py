4361877a99c66d6c76b08bb7332cf5efe0cfdb87
Lynkfox <lynkfox@gmail.com>
2021-05-24 23:59:28 -0400

    Settings Updated

--- entity_lambda/entity_dynamo_functions.py
+++ entity_lambda/entity_dynamo_functions.py
@@ -5,8 +5,9 @@ from boto3.dynamodb.conditions import Key
 from decimal import *
 import logging
 
+
 class EntityCommands():
-    def __init__(self, table_name:str, entity:str) -> None:
+    def __init__(self, table_name: str, entity: str) -> None:
         '''
         Contains the necessary information for a single entity and retrieving data for it
         '''
@@ -15,27 +16,25 @@ class EntityCommands():
         self.entity_name = entity
         self.entity_data = {}
         self.get_all_entity_data()
-    
+
     def get_all_entity_data(self) -> None:
         '''
         Retrieves all the info of an entity and collapses it into a single dictionary.
         '''
-        
+
         entity_items = self.entity_table.query(
             KeyConditionExpression=Key('pk').eq(f'ENTITY#{self.entity_name}')
         )['Items']
-        
-        
+
         # Extract the Various Items belonging to this Entity
         self.get_meta_data(entity_items[0])
-        
-        
+
         self.calculate_total_games(self.entity_data.get('total_wins'), self.entity_data.get('total_losses'))
-        
-    def get_meta_data(self, meta_data:dict) -> None:
+
+    def get_meta_data(self, meta_data: dict) -> None:
         '''
         Takes the MetaData of a given entity and translates it to the entity_data.
-        
+
         Parameters:
             meta_data(Dictionary): The [0] index of the dynamodby Items response.
         '''
@@ -44,16 +43,13 @@ class EntityCommands():
             if key == 'pk' or key == 'sk':
                 continue
             elif isinstance(meta_data[key], Decimal):
-                self.entity_data.update({ key: int(meta_data[key])})
-            else:  
-                self.entity_data.update({ key: meta_data[key] })
-        
-        
+                self.entity_data.update({key: int(meta_data[key])})
+            else:
+                self.entity_data.update({key: meta_data[key]})
+
     def calculate_total_games(self, *args) -> None:
         '''
         Quick Sum the Args
         '''
         logging.info('[entity_lambda.EntityCommands] - Calculating Total Games')
         self.entity_data['total_games'] = sum(list(args))
-        
-        
